2b5ef6948378f993260a7f28111b61279a557f36
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-02-07 13:08:25 -0300

    UserContainer height fix

--- src/components/UserContainer.vue
+++ src/components/UserContainer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts"></script>
 <template>
-  <section class="h-[calc(100dvh-149.5px)] md:h-[calc(100vh-88px)] bg-section flex justify-center items-center p-4">
+  <section
+    class="min-h-[calc(100dvh-149.5px)] md:min-h-[calc(100vh-88px)] bg-section flex justify-center items-center p-4">
     <slot></slot>
   </section>
 </template>
