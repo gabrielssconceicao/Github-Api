64b0d6f1abf2c36245e65b80eb675016b6d9b5c5
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-02-09 10:43:42 -0300

    adding repos route

---
+++ src/views/ReposView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Back</RouterLink>
+    </nav>
+    <h1>t Repos</h1>
+  </header>
+  <section>
+    Repos
+  </section>
+</template>
+
+
