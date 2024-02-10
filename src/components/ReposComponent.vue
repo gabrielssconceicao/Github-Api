d8413adb5303b5866b02969c55fac5549ae7c22f
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-02-10 13:15:46 -0300

    ReposComponent style

---
+++ src/components/ReposComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { type IRepos } from '@/types/repos'
+import { getLangColor } from '@/helpers/getLangColor'
+const props = defineProps<{ repos: IRepos }>()
+</script>
+<template>
+  <div :style="{ '--lang-color': getLangColor(props.repos.language) }" class="bg-zinc-600 p-3 relative max-w-[300px] max-h-[150px] rounded-xl shadow-md
+  before:w-2 before:absolute before:bg-[var(--lang-color)] before:top-0 before:left-0 before:bottom-0 before:rounded-tl-xl before:rounded-bl-xl
+   md:text-xl md:w-[500px]">
+    <h3 class="text-center p-1 font-bold font-serif">{{ props.repos.name }}</h3>
+    <p class="text-justify font-mono text-base break-words mt-3 mb-2" v-if="props.repos.description">{{
+      props.repos.description }}</p>
+    <div class="flex justify-between p-2">
+      <p class="px-2 py-1 font-sans border-2 border-[var(--lang-color)] text-[var(--lang-color)]">{{ props.repos.language
+      }}
+      </p>
+      <a :href="props.repos.html_url" target="_blank" class="border-white border-2  px-2 py-1 text-white">See Repo</a>
+    </div>
+  </div>
+</template>
--- src/components/RepositoriesContainer.vue
+++ src/components/RepositoriesContainer.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { user, repos } from '@/mock'
-import ReposHeader from './ReposHeader.vue';
 
+import ReposHeader from './ReposHeader.vue';
 import ReposFilter from './ReposFilterComponent.vue';
+import ReposComponent from './ReposComponent.vue';
+
 import { langSeparator } from '@/helpers/langSeparator';
 
 const filterValue = ref('');
 const langsRepos = computed(() => langSeparator(repos));
+//Fix filter for Java and JavaScript
 const filteredRepos = computed(() => repos.filter(repo => repo.language.includes(filterValue.value)));
 
 const getFilter = (value: string) => {
@@ -20,14 +23,8 @@ const getFilter = (value: string) => {
   <ReposHeader :name="user.name || user.login" />
 
   <ReposFilter :filters="langsRepos" @filter="getFilter" />
-  <section class="h-[calc(100dvh-51.2px)]">
-    <div v-for="repo in filteredRepos" :key="repo.id">
-      <h3>{{ repo.name }}</h3>
-      <p>{{ repo.description }}</p>
-      <div>
-        <p>{{ repo.language }}</p>
-        <a :href="repo.html_url" target="_blank">See Repo</a>
-      </div>
-    </div>
+  <section class="h-[calc(100dvh-51.2px)]" id="repos">
+    <ReposComponent v-for="repo in filteredRepos" :key="repo.id" :repos="repo" />
   </section>
 </template>
+<style scoped></style>
